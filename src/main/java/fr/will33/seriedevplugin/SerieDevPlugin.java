d433f50b76d98e09e3b429038751f82fd4fc4618
Will33_ <william.aventin@elesia.org>
2023-07-26 13:53:01 +0200

    Episode n°1 de la série

---
+++ src/main/java/fr/will33/seriedevplugin/SerieDevPlugin.java
@@ -0,0 +1,11 @@
+package fr.will33.seriedevplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SerieDevPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
